8d4d2026f3793eab820d290eaec5b5dcb48f3182
tangbo <852425209@qq.com>
2024-01-30 10:49:12 +0800

    修订版

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,20 +2,16 @@
  * @Author       : tangbo 852425209@qq.com
  * @Date         : 2022-06-29 16:54:07
  * @LastEditors  : tangbo 852425209@qq.com
- * @LastEditTime : 2023-08-24 11:08:18
+ * @LastEditTime : 2023-08-11 10:14:22
  * @FilePath     : \vue3_ts\src\pages\index\index.vue
  * @Description  : 首页
 -->
 
 <template>
-  <div>
-    <open-layers />
-  </div>
+  <div class="test">1111</div>
 </template>
 
-<script lang="ts" setup>
-import OpenLayers from '/@/components/OpenLayers'
-</script>
+<script lang="ts" setup></script>
 
 <style lang="less" scoped>
 @import './index.less';
