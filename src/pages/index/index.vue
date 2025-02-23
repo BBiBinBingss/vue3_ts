fb3d2ba2660f7d8cb06438429d93ace52b600fa2
BBiBinBings <852425209@qq.com>
2025-02-23 22:40:42 +0800

    修改写法

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,9 @@
 <!--
  * @Author       : tangbo 852425209@qq.com
  * @Date         : 2022-06-29 16:54:07
- * @LastEditors  : tangbo 852425209@qq.com
- * @LastEditTime : 2023-08-24 11:08:18
- * @FilePath     : \vue3_ts\src\pages\index\index.vue
+ * @LastEditors: BBiBinBings 
+ * @LastEditTime: 2025-02-23 22:36:18
+ * @FilePath: \vue3_tsv\src\pages\index\index.vue
  * @Description  : 首页
 -->
 
@@ -15,6 +15,8 @@
 
 <script lang="ts" setup>
 import OpenLayers from '/@/components/OpenLayers'
+
+let i = $ref(0)
 </script>
 
 <style lang="less" scoped>
